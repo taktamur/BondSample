1431fb10121f2bd416549dd8559425cac3447923
Takafumi Tamura <taktamur@gmail.com>
2016-03-06 14:58:26 +0900

    Initial Commit

---
+++ BondSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BondSample
+//
+//  Created by 田村孝文 on 2016/03/06.
+//  Copyright © 2016年 田村孝文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
